e03041d1a804df50b48a300fcd7db598417ef0ea
Sébastien Fauvel <sfauvel@gmail.com>
2017-11-16 00:09:59 +0100

    Add stylesheet and rewrite some parts

--- generate.sh
+++ generate.sh
@@ -1,1 +1,6 @@
-asciidoctor index.adoc
+#!/bin/bash
+
+asciidoctor -a linkcss -a stylesheet=catestdrale.css -a stylesdir=. *.adoc
+
+asciidoctor -r asciidoctor-pdf -b pdf -o catestdrale.pdf *.adoc
+
